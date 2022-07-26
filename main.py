a4de1f364f85999e3a1a5b7e79d3ff41e35f47c0
Phoneria <90638359+Phoneria@users.noreply.github.com>
2022-07-26 13:02:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,6 @@ def tg_writer(coin_name):
     interval = Client.KLINE_INTERVAL_30MINUTE
 
     create_csv(coin_name, bring_data(coin_name, interval, str(start_time), str(finish_time)))
-    
-    
 
 
 def candle_stick(coin_name):
@@ -59,109 +57,11 @@ def candle_stick(coin_name):
 
     def calculate_time(number):
         return dt.fromtimestamp(open_time.iloc[number] / 1000)
-    
-    pivot_list = list()
-    def pivot():
-        High = 0
-        Low = 10000
-        Close = 0
-
-        current_time = len(close_level) - 1
-
-
-        if (calculate_time(current_time).hour == 3 and calculate_time(current_time).minute == 0):
-
-            current_time -= 1
-            for i in range(49):
-
-                if high_level[current_time - i] > High:
-                    High = high_level[current_time - i]
-                if low_level[current_time - i] < Low:
-                    Low = low_level[current_time - i]
-
-                if i == 0:
-                    Close = close_level[current_time - i]
-
-            PP = (High + Low + Close) / 3
-            R1 = 2 * PP - Low
-            S1 = 2 * PP - High
-            R2 = PP + (High - Low)
-            S2 = PP - (High - Low)
-            R3 = 2 * PP + (High - 2 * Low)
-            S3 = 2 * PP - (2 * High - Low)
-            R4 = 3 * PP + (High - 3 * Low)
-            S4 = 3 * PP - (3 * High - Low)
-            R5 = 4 * PP + (High - 4 * Low)
-            S5 = 4 * PP - (4 * High - Low)
-
-            pivot_list.clear()
-
-            pivot_list.append(PP)
-            pivot_list.append(R1)
-            pivot_list.append(R2)
-            pivot_list.append(R3)
-            pivot_list.append(R4)
-            pivot_list.append(R5)
-            pivot_list.append(S1)
-            pivot_list.append(S2)
-            pivot_list.append(S3)
-            pivot_list.append(S4)
-            pivot_list.append(S5)
-
-        return pivot_list
-
-    def rsi_indicator():
-        rsi = ta.rsi(close=close_level, length=14)
-
-        if rsi.iloc[len(close_level) - 1] > 70:
-            message("\nOVERBOUGHT!!!\nCURRENT RSI VALUE : "+ str(rsi.iloc[len(close_level) - 1])+"\n" +
-             str( calculate_time(len(close_level) - 1)))
-
-        if rsi.iloc[len(close_level) - 1] < 30:
-             message("\nOVERSOLD!!!\nCURRENT RSI VALUE : "+str( rsi.iloc[len(close_level) - 1])+"\n" +str (calculate_time(len(close_level) - 1)))
-
-
 
     max_level = 0
     min_level = 1000000000
-    
-    
     for i in range(5, len(close_level)):
-        rsi_indicator()
-        pivot()
-        
-         if len(pivot_list > 2):
 
-            if close_level[i-1] < pivot_list[0] and close_level[i] > pivot_list[0]:
-                message("PIVOT ÇİZGİSİNİ YUKARI KIRDI")
-            if close_level[i-1] < pivot_list[1] and close_level[i] > pivot_list[1]:
-                message("R1 ÇİZGİSİNİ NOKTASINI YUKARI KIRDI")
-            if close_level[i-1] < pivot_list[2] and close_level[i] > pivot_list[2]:
-                message("R2 ÇİZGİSİNİ NOKTASINI YUKARI KIRDI")
-            if close_level[i-1] < pivot_list[3] and close_level[i] > pivot_list[3]:
-                message("R3 ÇİZGİSİNİ NOKTASINI YUKARI KIRDI")
-            if close_level[i-1] < pivot_list[4] and close_level[i] > pivot_list[4]:
-                message("R4 ÇİZGİSİNİ NOKTASINI YUKARI KIRDI")
-            if close_level[i-1] < pivot_list[5] and close_level[i] > pivot_list[5]:
-                message("R5 ÇİZGİSİNİ NOKTASINI YUKARI KIRDI")
-
-            if close_level[i-1] > pivot_list[0] and close_level[i] < pivot_list[0]:
-                message("PIVOT ÇİZGİSİNİ AŞAĞI KIRDI")
-            if close_level[i - 1] > pivot_list[6] and close_level[i] < pivot_list[6]:
-                message("S1 ÇİZGİSİNİ AŞAĞI KIRDI")
-            if close_level[i - 1] > pivot_list[7] and close_level[i] < pivot_list[7]:
-                message("S2 ÇİZGİSİNİ AŞAĞI KIRDI")
-            if close_level[i - 1] > pivot_list[8] and close_level[i] < pivot_list[8]:
-                message("S3 ÇİZGİSİNİ AŞAĞI KIRDI")
-            if close_level[i - 1] > pivot_list[9] and close_level[i] < pivot_list[9]:
-                message("S4 ÇİZGİSİNİ AŞAĞI KIRDI")
-            if close_level[i - 1] > pivot_list[10] and close_level[i] < pivot_list[10]:
-                message("S5 ÇİZGİSİNİ AŞAĞI KIRDI")
-
-        
-        
-        
-        
         if calculate_time(i).hour == time.localtime().tm_hour and calculate_time(i).day == datetime.today().day:
             if max_level < high_level[i]:
                 max_level = high_level[i]
@@ -312,9 +212,8 @@ def candle_stick(coin_name):
                 max_level = high_level[i]
             if low_level[i] <= min_level:
                 min_level = low_level[i]
-
-
-
+                
+                
 def tg_writer_scalp(coin_name):
     client = Client(None, None)
 
@@ -345,22 +244,23 @@ def candle_stick_scalp(coin_name):
 
     df = pd.read_csv(coin_name + "1MINTG.csv", names=titles)
 
-
     close_level = df["Close"]
     open_time = df["Open Time"]
 
     def calculate_time(number):
         return dt.fromtimestamp(open_time.iloc[number] / 1000)
 
-
     rsi = ta.rsi(close=close_level, length=14)
 
     if rsi.iloc[len(close_level) - 1] > 70:
-        message("\nOVERBOUGHT!!!\nCURRENT RSI VALUE : "+ str(rsi.iloc[len(close_level) - 1])+"\n" +
-             str( calculate_time(len(close_level) - 1)))
+        message("\nOVERBOUGHT!!!\nCURRENT RSI VALUE : " + str(rsi.iloc[len(close_level) - 1]) + "\n" +
+                str(calculate_time(len(close_level) - 1)))
 
     if rsi.iloc[len(close_level) - 1] < 30:
-        message("\nOVERSOLD!!!\nCURRENT RSI VALUE : "+str( rsi.iloc[len(close_level) - 1])+"\n" +str (calculate_time(len(close_level) - 1)))
+        message("\nOVERSOLD!!!\nCURRENT RSI VALUE : " + str(rsi.iloc[len(close_level) - 1]) + "\n" + str(
+            calculate_time(len(close_level) - 1)))            
+
+
 
 
 
@@ -369,9 +269,11 @@ message("YENİDEN BAŞLADI")
 
 while True:
     try:
+        
+        
         if (time.localtime().tm_sec == 59):
             candle_stick_scalp("NEARUSDT")
-            
+        
         if (time.localtime().tm_min == 29 or time.localtime().tm_min == 59) and time.localtime().tm_sec == 50:
             candle_stick("NEARUSDT")
             candle_stick("BTCUSDT")
